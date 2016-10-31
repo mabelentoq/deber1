3dba0521d926869f63766b136553b985295af36a
mabelentoq <mbtoaquiza@espe.edu.ec>
2016-10-30 23:44:01 -0500

    Update deber.R

--- deber.R
+++ deber.R
@@ -1,2 +1,22 @@
-makeCacheMatrix<-matrix(1:16,4,4)
-cacheSolve<-solve(makeCacheMatrix)
+##  Prueba
+##calcular la funcion para una matriz establecida en la funcion
+m<-rbind(c(4, 3), c(4, 3))  
+m
+makeCacheMatrix(m)
+inv_matrix
+
+##funcion para la inversa de la matriz     
+cacheSolve <- function(x, ...) {
+  if(!is.null(inv_matrix)) {
+    message("getting cached data")
+    b <- inv_matrix
+  }
+  else 	{
+    b<-solve(x)
+  }
+  b
+}
+
+##Segunda Prueba 
+cacheSolve(m)
+
